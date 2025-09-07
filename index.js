01610902eee3af8c228412585476124efa8a34bd
FatimataZahra <diafatima1408@gmail.com>
2025-09-07 11:16:54 +0000

    some changes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Gestion simple du formulaire
 document.getElementById("contactForm").addEventListener("submit", function (e) {
   e.preventDefault();
   alert("Merci pour votre message ! Je vous répondrai bientôt.");
@@ -10,4 +9,14 @@ const nav = document.querySelector("header nav");
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-active");
+});
+
+const navLinks = document.querySelectorAll("header nav a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (window.innerWidth <= 768) {
+      nav.classList.remove("nav-active");
+    }
+  });
 });
